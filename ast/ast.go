7398a52de0773dec8f4cf786857fea6c7b54536e
Daniel Shifman <Daniel.Shifman@carleton.ca>
2019-07-21 21:53:15 -0400

    added boolean parsing

--- ast/ast.go
+++ ast/ast.go
@@ -154,6 +154,19 @@ func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
 }
 
+type Boolean struct {
+	Token token.Token // Boolean Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
+
 func (p *Program) String() string {
 	var out bytes.Buffer
 	for _, s := range p.Statements {
