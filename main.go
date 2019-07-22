ae8436997c4585c61182b7327b393881701cfbec
Daniel Shifman <Daniel.Shifman@carleton.ca>
2019-07-22 06:55:34 -0400

    added parsing functionality and Splash to REPL.

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ import (
 	"os/user"
 )
 
+const SPLASH = `
+ ___ _  _____  __ __
+|   | ||_   _||  |  |
+| | | | _| |_ |  |  |
+|_|___||_____||_____|REPL
+`
+
 func main() {
 	usr, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! Welcome to the NIU REPL!\n", usr.Username)
+	fmt.Printf(SPLASH)
+	fmt.Printf("\n\nHello %s! Welcome to the NIU REPL!\n", usr.Username)
 	fmt.Printf("Enter commands:\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
