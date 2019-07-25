cc1505d346ee0ed35dc6168ac9c4f71f2500a1f1
Daniel Shifman <daniel.shifman@carleton.ca>
2019-07-25 13:01:06 -0400

    added function closure

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -202,6 +202,15 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosure(t *testing.T) {
+	input := `let newAdder = fn(x) {
+				fn(y) { x + y; };
+				};
+				let addTwo = newAdder(2);
+				addTwo(2);`
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
